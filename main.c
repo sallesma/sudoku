e8cf062823d14c52c02327b39834aea2e7233e7c
Martin Salles <martin.salles@free.fr>
2014-07-08 19:57:20 +0200

    Display sudoku

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define GLOBAL_SIZE 81
+#define LINE_LENGTH 9
+
 int* load_sudoku() {
-	int[9, 9]sudoku;
+	static int sudoku[GLOBAL_SIZE];
+	int i, j;
+    for(i=0 ; i<LINE_LENGTH ; i++)
+		for(j=0 ; j<LINE_LENGTH ; j++) {
+            printf("%d", j);
+			*(sudoku+i*LINE_LENGTH+j) = j;
+        }
 	return sudoku;
 }
-int main(int argc, char** argv) {
 
+void display_sudoku(int * sudoku) {
+	int i, j;
+	for(i=0 ; i<LINE_LENGTH ; i++) {
+		for(j=LINE_LENGTH ; j<LINE_LENGTH ; j++)
+			printf("%d ", *(sudoku+i*LINE_LENGTH+j));
+		printf("\n");
+	}
+}
+
+int main(int argc, char** argv) {
+	int * s = load_sudoku();
+	display_sudoku(s);
 	return 0;
 }
