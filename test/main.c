0af9627551c922b4fbea9bbde6f26809f18e80ae
Martin Salles <martin.salles@free.fr>
2015-03-04 20:24:26 +0100

    Add functional tests with known sudokus

--- test/main.c
+++ test/main.c
@@ -3,6 +3,12 @@
 #include "sudoku.h"
 #include "basic.h"
 #include "exhaustive.h"
+#include "../src/constants.h"
+#include "../src/sudoku.h"
+#include "../src/basic.h"
+#include "../src/exhaustive.h"
+
+void test_functional(void);
 
 int main()
 {
@@ -24,6 +30,12 @@ int main()
 
    CU_pSuite pSuiteExhaustive = CU_add_suite("Suite Exhaustive", NULL, NULL);
    if (NULL == pSuiteExhaustive) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   CU_pSuite pSuiteFunctional = CU_add_suite("Suite Functional", NULL, NULL);
+   if (NULL == pSuiteFunctional) {
       CU_cleanup_registry();
       return CU_get_error();
    }
@@ -53,10 +65,34 @@ int main()
       return CU_get_error();
    }
 
+   if (NULL == CU_add_test(pSuiteFunctional, "test functional", test_functional))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+
 
    /* Run all tests using the CUnit Basic interface */
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
    CU_cleanup_registry();
    return CU_get_error();
+}
+
+void test_functional(void)
+{
+   int sudoku[GLOBAL_SIZE];
+   load_sudoku(sudoku, "sudoku_basic.txt");
+   
+   CU_ASSERT(solve_sudoku_basic(sudoku));
+   CU_ASSERT(solve_sudoku_exhaustive(sudoku));
+
+   load_sudoku(sudoku, "sudoku_exhaustive.txt");
+   CU_ASSERT_FALSE(solve_sudoku_basic(sudoku));
+   CU_ASSERT(solve_sudoku_exhaustive(sudoku));
+
+   load_sudoku(sudoku, "sudoku_empty.txt");
+   CU_ASSERT_FALSE(solve_sudoku_basic(sudoku));
+   CU_ASSERT(solve_sudoku_exhaustive(sudoku));
 }
