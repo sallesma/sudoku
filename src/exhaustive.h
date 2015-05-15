777b376f253dfc3f4adbdcf1523efd3f14646149
Martin Salles <martin.salles@free.fr>
2015-05-15 15:31:42 +0200

    Remove useless function declaration

--- src/exhaustive.h
+++ src/exhaustive.h
@@ -5,8 +5,6 @@ int get_first_empty(int * sudoku);
 
 int get_next_possible_value(int * possibilities, int box_index, int previous);
 
-int update_value(int* sudoku, int* possibilities, int index, int value);
-
 int solve_sudoku_exhaustive(int * sudoku);
 
 #endif
